95b34a7c6172a4f140b473fa65ca3c2d12fed1b9
Samson Adegboye <adegboye72@gmail.com>
2020-12-18 06:54:47 +0100

    server mods.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-const publicPath = path.join(__dirname, '../public');
+// const publicPath = path.join(__dirname, 'build');
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
