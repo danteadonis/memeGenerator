9d3d45dbbd81e4bdef071ff7dfffd716a2afd82a
Samson Adegboye <adegboye72@gmail.com>
2020-12-18 08:42:42 +0100

    server mods.

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3001;
 
 // const publicPath = path.join(__dirname, 'build');
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(port, () => {
